aa140e1355bc19d2d88605cbd924a035fc8ca031
IvoRum <ivailo.rumenov@outlook.com>
2024-02-20 22:49:37 +0200

    Created starter project

---
+++ terra-server/src/main/java/com/terra/server/TerraServerApplication.java
@@ -0,0 +1,13 @@
+package com.terra.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TerraServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TerraServerApplication.class, args);
+    }
+
+}
---
+++ terra-server/src/test/java/com/terra/server/TerraServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.terra.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TerraServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
