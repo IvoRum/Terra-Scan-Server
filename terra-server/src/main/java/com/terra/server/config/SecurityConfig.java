cc5326a2ea21679dd96bb58e8acdac0328f745fc
IvoRum <ivailo.rumenov@outlook.com>
2024-02-28 21:43:41 +0200

    Created new service for authenticating user and returning a token

--- terra-server/src/main/java/com/terra/server/config/SecurityConfig.java
+++ terra-server/src/main/java/com/terra/server/config/SecurityConfig.java
@@ -37,7 +37,6 @@ public class SecurityConfig {
         http
                 .cors().and()
                 .csrf().disable()
-
                 .sessionManagement()
                 .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 .and()
