d592ef7073048c06b60b482f2c0b486e795b1852
IvoRum <ivailo.rumenov@outlook.com>
2024-03-19 00:09:24 +0200

    Working on soil

--- terra-server/src/main/java/com/terra/server/model/responce/SoilDTO.java
+++ terra-server/src/main/java/com/terra/server/model/responce/SoilDTO.java
@@ -2,6 +2,9 @@ package com.terra.server.model.responce;
 
 import lombok.Data;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 @Data
 public class SoilDTO {
     private float lon;
