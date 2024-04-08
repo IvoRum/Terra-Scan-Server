11abe75f623a0a7e5f0a3ffd63c1eb8d1280734a
NikolayStaykov <nikolay.staykov98@abv.bg>
2024-04-08 22:41:42 +0300

    Added functionality for logging searches by country

--- terra-server/src/main/java/com/terra/server/service/EarthquakeService.java
+++ terra-server/src/main/java/com/terra/server/service/EarthquakeService.java
@@ -1,12 +1,18 @@
 package com.terra.server.service;
 
+import com.mongodb.client.MongoClients;
 import com.terra.server.model.request.EarthquakeRequest;
 import com.terra.server.model.responce.EarthquakeResponse;
+import com.terra.server.persistence.TerraSearchLogEntity;
 import com.terra.server.repository.EarthquakeRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.convert.ReadingConverter;
+import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.sql.Date;
+import java.time.Instant;
 
 
 @Service
@@ -15,9 +21,22 @@ import org.springframework.transaction.annotation.Transactional;
 @RequiredArgsConstructor
 public class EarthquakeService {
     private final EarthquakeRepository repository;
+    private static final String CONNECTION_STRING = "mongodb+srv://ivo:ar4ebar4e@terra-log.rumnftg.mongodb.net/?retryWrites=true&w=majority&appName=terra-log";
+
 
     public EarthquakeResponse getEarthquakes(EarthquakeRequest request) {
         var earthquakes = repository.getEarthquakes(request.getLatitude(),request.getLongitude(),request.getRadius());
+        String country = repository.getCountryOfPoint(request.getLatitude(),request.getLongitude());
+        if(country != null){
+            MongoTemplate mongoTemplate =
+                    new MongoTemplate
+                            (MongoClients.create(String.format(CONNECTION_STRING)), "prod");
+            var log = new TerraSearchLogEntity();
+            log.setCountry(country);
+            log.setDate(Date.from(Instant.now()));
+            log.setSearchType("Earthquake");
+            mongoTemplate.insert(log);
+        }
         return EarthquakeResponse.builder()
                 .allEarthquakesInArea(earthquakes)
                 .build();
--- terra-server/src/main/java/com/terra/server/service/SoilService.java
+++ terra-server/src/main/java/com/terra/server/service/SoilService.java
@@ -2,6 +2,8 @@ package com.terra.server.service;
 
 import com.mongodb.client.MongoClients;
 import com.terra.server.model.responce.dto.SoilDTO;
+import com.terra.server.persistence.TerraSearchLogEntity;
+import com.terra.server.persistence.TerraUserLogEntity;
 import com.terra.server.repository.SoilRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.convert.ReadingConverter;
@@ -9,6 +11,8 @@ import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.sql.Date;
+import java.time.Instant;
 import java.util.List;
 
 @Service
@@ -25,6 +29,18 @@ public class SoilService {
     }
 
     public List<SoilDTO> getSoilInAria(final double latitude, final double longitude, final double zoom){
-        return repository.getSoil(latitude, longitude, zoom);
+        var res = repository.getSoil(latitude, longitude, zoom);
+        String country = repository.getCountryOfPoint(latitude, longitude);
+        if(country != null){
+            MongoTemplate mongoTemplate =
+                    new MongoTemplate
+                            (MongoClients.create(String.format(CONNECTION_STRING)), "prod");
+            var log = new TerraSearchLogEntity();
+            log.setCountry(country);
+            log.setDate(Date.from(Instant.now()));
+            log.setSearchType("Soil");
+            mongoTemplate.insert(log);
+        }
+        return res;
     }
 }
